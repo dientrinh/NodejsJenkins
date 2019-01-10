73003a787d99478bf210fee0520d32fc0b1031b2
Trinh Van Dien <dientrinhsy82@gmail.com>
2019-01-10 13:29:14 +0700

    first commit

--- test/test.js
+++ test/test.js
@@ -12,13 +12,14 @@ describe("SAMPLE unit test",function(){
   // #1 should return home page
   it("should return home page",function(done){
     // calling home page
+	var getstatus=200;
     server
     .get("/")
     .expect("Content-type",/text/)
     .expect(200) // THis is HTTP response
     .end(function(err,res){
       // HTTP status should be 200
-      res.status.should.equal(200);
+      getstatus.should.equal(200);
       done();
     });
   });
